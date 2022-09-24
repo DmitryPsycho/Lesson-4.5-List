563b19c9d560d0fb7b65d7e3c1f58353f5180dd8
DmitryPsycho <kovalenko2511@bk.ru>
2022-09-24 13:00:24 +0300

    Initial Commit

---
+++ Lesson 4.5 List/Lesson_4_5_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_4_5_ListApp.swift
+//  Lesson 4.5 List
+//
+//  Created by Psycho on 24.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_4_5_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
